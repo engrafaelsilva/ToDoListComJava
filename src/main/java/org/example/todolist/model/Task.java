c0b07feeddc2e20b8193358258c54c48c4c441dd
Rafael Silva <rafael_develloper@hotmail.com>
2025-09-06 15:39:14 -0300

    Update Task.java

--- src/main/java/org/example/todolist/model/Task.java
+++ src/main/java/org/example/todolist/model/Task.java
@@ -1,4 +1,26 @@
 package org.example.todolist.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    private String titulo;
+    private String descricao;
+    private boolean concluida;
+    private LocalDateTime criadaEm;
+
 }
